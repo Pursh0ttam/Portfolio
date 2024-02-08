c53bee945fbb9d73d3ddf898ae488ef3b925257a
Purshottam <kumarpurshottam474@gmail.com>
2024-02-08 20:52:40 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import './index.css'
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import SocialLinks from './components/SocialLinks';
-import Home2 from './components/home2';
+import Home2 from './components/Home2.jsx';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
+
 
 
 function App() {
